687dcb4a7d5f14c066d29c719de3af8eac8a24e9
Al-Amin018555 <alamin15-3799@diu.edu.bd>
2025-08-27 17:00:43 +0600

    margin issue fixed in blog tabs

--- src/pages/Blog.jsx
+++ src/pages/Blog.jsx
@@ -21,7 +21,7 @@ const Blog = () => {
                     </div>
                 </div>
                 {/* tabs */}
-                <div className="flex items-center -mx-4 overflow-x-auto overflow-y-hidden sm:justify-start flex-nowrap dark:bg-gray-100 dark:text-gray-800">
+                <div className="flex items-center overflow-x-auto overflow-y-hidden sm:justify-start flex-nowrap dark:bg-gray-100 dark:text-gray-800">
                     <Link to='' onClick={() => setTabIndex(0)} className={`flex items-center flex-shrink-0 px-5 py-3 space-x-2 ${tabIndex === 0 ? 'border border-b-0': 'border-b'} rounded-t-lg dark:border-gray-600 dark:text-gray-900`}>
                         <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" className="w-4 h-4">
                             <path d="M19 21l-7-5-7 5V5a2 2 0 0 1 2-2h10a2 2 0 0 1 2 2z"></path>
