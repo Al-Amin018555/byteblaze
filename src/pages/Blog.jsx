dff80b8bbf3ec87f9630e6a82644551b7c1a3d87
Al-Amin018555 <alamin15-3799@diu.edu.bd>
2025-08-27 16:55:44 +0600

    used conditional styling on the tab of blog

--- src/pages/Blog.jsx
+++ src/pages/Blog.jsx
@@ -1,8 +1,13 @@
+import { useState } from "react";
 import { Link, useLoaderData } from "react-router-dom";
 
 const Blog = () => {
+    const [tabIndex, setTabIndex] = useState(0);
     const blog = useLoaderData();
-    const { title, published_at, comments_count, public_reactions_count, reading_time_minutes } = blog;
+    const { title, published_at,
+        comments_count,
+        public_reactions_count,
+        reading_time_minutes } = blog;
     return (
         <div className="max-w-2xl px-6 py-16 mx-auto space-y-12">
             <article className="space-y-8 dark:bg-gray-100 dark:text-gray-900">
@@ -17,20 +22,21 @@ const Blog = () => {
                 </div>
                 {/* tabs */}
                 <div className="flex items-center -mx-4 overflow-x-auto overflow-y-hidden sm:justify-start flex-nowrap dark:bg-gray-100 dark:text-gray-800">
-                    <Link to='' className="flex items-center flex-shrink-0 px-5 py-3 space-x-2 border-b dark:border-gray-600 dark:text-gray-600">
+                    <Link to='' onClick={() => setTabIndex(0)} className={`flex items-center flex-shrink-0 px-5 py-3 space-x-2 ${tabIndex === 0 ? 'border border-b-0': 'border-b'} rounded-t-lg dark:border-gray-600 dark:text-gray-900`}>
                         <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" className="w-4 h-4">
                             <path d="M19 21l-7-5-7 5V5a2 2 0 0 1 2-2h10a2 2 0 0 1 2 2z"></path>
                         </svg>
                         <span>Content</span>
                     </Link>
-                    <Link to='' className="flex items-center flex-shrink-0 px-5 py-3 space-x-2 border border-b-0 rounded-t-lg dark:border-gray-600 dark:text-gray-900">
+
+                    <Link to='' onClick={() => setTabIndex(1)} className={`flex items-center flex-shrink-0 px-5 py-3 space-x-2 ${tabIndex === 1 ? 'border border-b-0': 'border-b'} rounded-t-lg dark:border-gray-600 dark:text-gray-900`}>
                         <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" className="w-4 h-4">
                             <path d="M2 3h6a4 4 0 0 1 4 4v14a3 3 0 0 0-3-3H2z"></path>
                             <path d="M22 3h-6a4 4 0 0 0-4 4v14a3 3 0 0 1 3-3h7z"></path>
                         </svg>
                         <span>Author</span>
                     </Link>
-                    
+
                 </div>
             </article>
             <div>
