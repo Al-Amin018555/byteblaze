cb24c2bb8da61faaf00b016133c8af40f5909adc
Al-Amin018555 <alamin15-3799@diu.edu.bd>
2025-08-27 23:55:01 +0600

    added some layout on blog children content

--- src/components/Author.jsx
+++ src/components/Author.jsx
@@ -1,7 +1,11 @@
+import { useLoaderData } from "react-router-dom";
+
 const Author = () => {
+     const blog = useLoaderData();
+     const {cover_image,title,description,published_at,user} = blog;
     return (
         <div>
-            <h1>Ami author na</h1>            
+            <h1>{user.name}</h1>            
         </div>
     );
 };
--- src/components/Content.jsx
+++ src/components/Content.jsx
@@ -1,8 +1,21 @@
+import { useLoaderData } from 'react-router-dom';
+import placeholderImage from '../assets/404.jpg'
 const Content = () => {
+    const blog = useLoaderData();
+     const {cover_image,title,description,published_at,tags} = blog;
+     console.log(blog)
     return (
-        <div>
-            <h1>Ami ekjon content</h1>            
-        </div>
+        <div className="mx-auto p-2 border-1 border-gray-400 group hover:no-underline focus:no-underline dark:bg-gray-50">
+                <img role="presentation" className="object-cover w-full rounded h-44 dark:bg-gray-500" src={cover_image || placeholderImage} />
+                 <div className="flex flex-wrap py-6 gap-2 border-t border-dashed dark:border-gray-600">
+                    {
+                        tags.map(tag =><a key={tag} rel="noopener noreferrer" href="#" className="px-3 py-1 rounded-sm hover:underline dark:bg-violet-600 dark:text-gray-50">#{tag}</a>)
+                    }
+
+                </div>
+               
+            </div>
+       
     );
 };
 
