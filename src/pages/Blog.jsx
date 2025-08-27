315eb0cd17a4bab4f9656bc3ce7fd9e9be613b43
Al-Amin018555 <alamin15-3799@diu.edu.bd>
2025-08-27 17:38:29 +0600

    created nested routes to the blog

--- src/pages/Blog.jsx
+++ src/pages/Blog.jsx
@@ -1,5 +1,5 @@
 import { useState } from "react";
-import { Link, useLoaderData } from "react-router-dom";
+import { Link, Outlet, useLoaderData } from "react-router-dom";
 
 const Blog = () => {
     const [tabIndex, setTabIndex] = useState(0);
@@ -29,7 +29,7 @@ const Blog = () => {
                         <span>Content</span>
                     </Link>
 
-                    <Link to='' onClick={() => setTabIndex(1)} className={`flex items-center flex-shrink-0 px-5 py-3 space-x-2 ${tabIndex === 1 ? 'border border-b-0': 'border-b'} rounded-t-lg dark:border-gray-600 dark:text-gray-900`}>
+                    <Link to='author' onClick={() => setTabIndex(1)} className={`flex items-center flex-shrink-0 px-5 py-3 space-x-2 ${tabIndex === 1 ? 'border border-b-0': 'border-b'} rounded-t-lg dark:border-gray-600 dark:text-gray-900`}>
                         <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" className="w-4 h-4">
                             <path d="M2 3h6a4 4 0 0 1 4 4v14a3 3 0 0 0-3-3H2z"></path>
                             <path d="M22 3h-6a4 4 0 0 0-4 4v14a3 3 0 0 1 3-3h7z"></path>
@@ -38,6 +38,7 @@ const Blog = () => {
                     </Link>
 
                 </div>
+                <Outlet></Outlet>
             </article>
             <div>
                 <div className="flex flex-wrap py-6 gap-2 border-t border-dashed dark:border-gray-600">
